80c3f3cef09c7a3a7653a2b25ed868b1cf3115a2
pooja.murarka <pooja.murarka16@gmail.com>
2013-09-06 10:29:35 +0530

    Included UTF8 format for encoding to solve error "com.sun.org.apache.xerces.internal.impl.io.MalformedByteSequenceException: Invalid byte 3 of 3-byte UTF-8 sequence."

--- RssFeed/src/com/feed/RSSFeed.java
+++ RssFeed/src/com/feed/RSSFeed.java
@@ -3,9 +3,11 @@ package com.feed;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
@@ -151,8 +153,9 @@ public class RSSFeed {
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					yc.getInputStream()));
 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
+			//FileOutputStream fw = new FileOutputStream(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"UTF-8"));
+
 			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				bw.write(inputLine);
---
+++ RssFeed/src/com/feed/Test.java
@@ -0,0 +1,15 @@
+package com.feed;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Microsoft's".contains("Microsoft"));
+		
+
+	}
+
+}
